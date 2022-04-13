dae12ac61fb6e1d660bb57a470b9b246f22943bb
ShadowPuppet66 <andrew@solydaria.co>
2022-04-13 00:41:08 -0400

    got testing

--- contracts/SingleItemSolyd.sol
+++ contracts/SingleItemSolyd.sol
@@ -21,7 +21,6 @@ contract SingleItemSolyd {
 	uint purchases = 0;
 	uint itemsSold = 0;
 	struct PurchaseLedger {
-		uint id;
 		uint timeStamp;
 		uint orderNumber;
 		uint shopOrderId;
@@ -29,7 +28,7 @@ contract SingleItemSolyd {
 		uint pricePaid;
 	}
 	mapping (uint => PurchaseLedger) purchaseId;
-	mapping (uint => PurchaseLedger) orderNumber;
+	mapping (uint => uint) orderNumber;
 	// set up player ledger 
 	uint players = 0;
 	struct PlayerLedger {
@@ -82,29 +81,33 @@ contract SingleItemSolyd {
 	}
 	// ➡️ Purchase Ledger Functions ➡️
 	// new purchase write to PurchaseLedger returns purchase id 
-	function writePurchase(uint _timeStamp, uint _orderNumber, uint _shopOrderId, uint _itemCount, uint _pricePaid) public returns (uint) {
+	function writePurchase(uint _timeStamp, uint _orderNumber, uint _shopOrderId, uint _itemCount, uint _pricePaid) public {
 
+		/*
 		// Check if order already is in ledger
-		if ( orderNumber[_orderNumber].orderNumber != 0 ){
+		if ( orderNumber[_orderNumber] == 0 ){
 		// Check for max items  
 			if ( itemsSold < maxItems ) {
 				uint id = purchases++;
-				itemsSold = itemsSold + _itemCount;
-				purchaseId[id] = PurchaseLedger(id, _timeStamp, _orderNumber, _shopOrderId, _itemCount, _pricePaid);
-				orderNumber[_orderNumber] = PurchaseLedger(id, _timeStamp, _orderNumber, _shopOrderId, _itemCount, _pricePaid);
-
-				return id;
-			} else { return 0;}
-		} else { return 0;}
+				itemsSold += _itemCount;
+				purchaseId[id] = PurchaseLedger(_timeStamp, _orderNumber, _shopOrderId, _itemCount, _pricePaid);
+				orderNumber[_orderNumber] = id;
+			} 
+		} 
+		*/
+		uint id = purchases++;
+		itemsSold += _itemCount;
+		purchaseId[id] = PurchaseLedger(_timeStamp, _orderNumber, _shopOrderId, _itemCount, _pricePaid);
+		orderNumber[_orderNumber] = id;
 	}
 	// get full purchase ledger 
 	function purchaseLedger() public view returns (uint[] memory, uint[] memory, uint[] memory, uint[] memory, uint[] memory) {
 		
-		uint[] memory timeStamp_;
-		uint[] memory orderNumber_;
-		uint[] memory shopOrderId_;
-		uint[] memory itemCount_;
-		uint[] memory pricePaid_;
+		uint[] memory timeStamp_ = new uint[](purchases);
+		uint[] memory orderNumber_ = new uint[](purchases);
+		uint[] memory shopOrderId_ = new uint[](purchases);
+		uint[] memory itemCount_ = new uint[](purchases);
+		uint[] memory pricePaid_ = new uint[](purchases);
 
 		for (uint i = 0; i < purchases; i++) {
 			PurchaseLedger memory entry = purchaseId[i];
@@ -121,7 +124,7 @@ contract SingleItemSolyd {
 	//link wallet to player ledger return player id, new player for every new nft minted
 	function newPlayer(uint _orderNumber, address _wallet0x, uint _nft) public returns (uint) {
 
-		uint purchaseId_ = orderNumber[_orderNumber].id;
+		uint purchaseId_ = orderNumber[_orderNumber];
 		
 		if ( purchaseId_ != 0) {
 			uint id = players++;
@@ -149,7 +152,7 @@ contract SingleItemSolyd {
 	//write payout return payout id
 	function writePayout(uint _timeStamp, uint _orderNumber, string memory _payoutMethod, string memory _payoutConfirmation, uint _payoutAmount ) public returns (uint) {
 
-		uint purchaseId_ = orderNumber[_orderNumber].id;
+		uint purchaseId_ = orderNumber[_orderNumber];
 		if ( purchaseId_ != 0) {
 			uint id = payments++;
 			payoutId[id] = PayoutLedger(_timeStamp, purchaseId_, _payoutMethod, _payoutConfirmation, _payoutAmount);
@@ -180,9 +183,9 @@ contract SingleItemSolyd {
 		uint[] memory pricePaid_ = new uint[](players);
 		uint[] memory notPaidoutPurchaseIds_ = notPaidoutPurchases();
 
-		for (uint i = 0; i < notPaidoutPurchaseIds_.length; i++) {
+		for (uint i = 1; i <= notPaidoutPurchaseIds_.length; i++) {
 			if (notPaidoutPurchaseIds_[i] != 0){
-				for (uint j = 0; j < players; j++) {
+				for (uint j = 1; j <= players; j++) {
 					if (playerId[j].purchaseLedgerId == i) {
 						wallets_[j] = playerId[j].walletOx;
 						pricePaid_[j] = (purchaseId[i].pricePaid);
@@ -202,7 +205,7 @@ contract SingleItemSolyd {
 		string[] memory payoutConfirmation_ = new string[](payments);
 		uint[] memory payoutAmount_ = new uint[](payments);
 
-		for (uint i = 0; i < payments; i++) {
+		for (uint i = 1; i <= payments; i++) {
 			timeStamp_[i]=payoutId[i].timeStamp;
 			orderNumber_[i] = purchaseId[payoutId[i].purchaseLedgerId].orderNumber;
 			payoutMedthod_[i] = payoutId[i].payoutMethod;
--- test/solyd-test.js
+++ test/solyd-test.js
@@ -5,21 +5,47 @@ const { ethers } = require("hardhat");
 chai.use(require('chai-bignumber')());
 
 describe("SingleItemSolyd", function() {
-	it("Should return 0 because no order in purchase ledger, or same order number if order in purchase ledger", async function () {
-		
-		const Solyd = await hre.ethers.getContractFactory("SingleItemSolyd");		
-		const solyd = await Solyd.deploy();
-		await solyd.deployed();
 
-		const setTx = await solyd.newPurchase(1648086691106, 1001, 1, 15);
-    	// wait until the transaction is mined
-    	await setTx.wait();
+	let Solyd;
+	let hardhatSolyd;
 
-		expect(await solyd.lookupOrder(1001)[0]).to.equal(1);
+	beforeEach(async function () {
 
-		// const entry = await solyd.lookupOrder(1001);
+		Solyd = await hre.ethers.getContractFactory("SingleItemSolyd");
+		hardhatSolyd = await Solyd.deploy();
 
-		// expect(await entry[2].to.equal(1001));
-		
+	});	
+
+	describe("Purchase Ledger", function () {
+
+		it("Should return 1 order in purchase ledger", async function () {
+			
+			await hardhatSolyd.writePurchase(1648086691106, 1001, 231244, 1, 1500);
+	    	
+	    	const purchaseLedger = await hardhatSolyd.purchaseLedger();
+	    	expect(purchaseLedger[1].length).to.equal(1);
+			
+		});
+
+		it("Should return 2nd order from purchase ledger", async function () {
+			
+			await hardhatSolyd.writePurchase(1648086691106, 1001, 231244, 1, 1500);
+			await hardhatSolyd.writePurchase(1648086691106, 1002, 231244, 1, 1500);
+	    	
+	    	const purchaseLedger = await hardhatSolyd.purchaseLedger();
+	    	expect(purchaseLedger[1][1]*1).to.equal(1002);
+			
+		});
+
+		it("Should return 5 units from purchase ledger", async function () {
+			
+			await hardhatSolyd.writePurchase(1648086691106, 1001, 231244, 2, 1500);
+			await hardhatSolyd.writePurchase(1648086691107, 1002, 231245, 1, 1500);
+			await hardhatSolyd.writePurchase(1648086691110, 1003, 231246, 2, 1500);
+	    	
+	    	const status = await hardhatSolyd.gameStatus();
+	    	expect(status[2] * 1).to.equal(5);
+			
+		});
 	});
 });
