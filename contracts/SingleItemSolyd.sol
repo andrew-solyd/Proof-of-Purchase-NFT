0c463fd773f379c281514bc5482fa6bd3b1b187f
ShadowPuppet66 <andrew@solydaria.co>
2022-05-06 13:04:52 -0400

    fixes for payouts

--- contracts/SingleItemSolyd.sol
+++ contracts/SingleItemSolyd.sol
@@ -8,16 +8,18 @@ contract SingleItemSolyd {
 	// set up contract states
 	bool private locked = false;
 	bool private live = false;
-	bool private stopped = false;
+	string private stopGameReason = "NA";
 	uint private launchTimestamp; 
 	// set up contract terms
 	string itemURL;
+	string escrowPaymentOrderId;
 	uint itemPrice;
 	uint maxItems;
 	uint maxCashbackPercent;
 	uint promoCashbackPercent;
 	uint gameLevels;
 	uint gameExpires;
+	uint stopTimestamp;
 	// set up purchase ledger
 	uint purchases = 0;
 	uint itemsSold = 0;
@@ -51,7 +53,7 @@ contract SingleItemSolyd {
 	mapping (uint => PayoutLedger) payoutId;
 	// ➡️ Contract Functions ➡️
 	// write terms and lock contract
-	function writeContractTerms (uint _launchTimestamp, string memory _itemURL, uint _itemPrice, uint _maxItems, uint _maxCashbackPercent, uint _promoCashbackPercent, uint _gameLevels, uint _gameExpires) public {
+	function writeContractTerms (uint _launchTimestamp, string memory _itemURL, uint _itemPrice, uint _maxItems, uint _maxCashbackPercent, uint _promoCashbackPercent, uint _gameLevels, uint _gameExpires, string memory _escrowPaymentOrderId) public {
 
 		if (locked == false) {
 			launchTimestamp = _launchTimestamp;
@@ -62,47 +64,55 @@ contract SingleItemSolyd {
 			promoCashbackPercent = _promoCashbackPercent;
 			gameLevels = _gameLevels;
 			gameExpires = _gameExpires;
+			escrowPaymentOrderId = _escrowPaymentOrderId;
 			locked = true;
 			live = true;
 		}
 	}
-	// start game
-	function startGame () public {
-		if (live == false && stopped == false) { live = true; }
-	}	
 	// stop game
-	function stopGame () public {
+	function stopGame (uint _stopTimestamp, string memory _stopGameReason) public {
 		if (live == true) { 
 			live = false;
-			stopped = true;
+			stopGameReason = _stopGameReason;
+			stopTimestamp = _stopTimestamp;
 		}
 	}
 	// get game status
-	function gameStatus () public view returns (uint, bool, uint, uint, uint, uint, uint, uint, uint, string memory) {
+	function gameStatus () public view returns (uint, bool, uint, uint, uint, uint, uint, uint, uint, string memory, uint, string memory) {
 
-		return (launchTimestamp, live, itemsSold, itemPrice, maxItems, maxCashbackPercent, promoCashbackPercent, gameLevels, gameExpires, itemURL);
+		return (launchTimestamp, live, itemsSold, itemPrice, maxItems, maxCashbackPercent, promoCashbackPercent, gameLevels, gameExpires, itemURL, stopTimestamp, stopGameReason);
+	}
+	function getEscrowPaymentOrderId() public view returns (string memory) {
+		return escrowPaymentOrderId;
 	}
 	// ➡️ Purchase Ledger Functions ➡️
 	// new purchase write to PurchaseLedger returns purchase id 
 	function writePurchase(uint _timeStamp, uint _orderNumber, uint _shopOrderId, uint _itemCount, uint _pricePaid) public {
-
-		/* 🚧 Need to make this check work!! 
-
-		// Check if order already is in ledger
-		if ( orderNumber[_orderNumber] == 0 ){
-		// Check for max items  
-			if ( itemsSold < maxItems ) {
+		
+		// check game is live
+		if ( live ) {
+		// check for duplicate orders
+			bool dupe = false;
+			for (uint i = 0; i < purchases; i++) {
+				if ( purchaseId[i].orderNumber == _orderNumber ) { dupe = true; }
+			}
+			if ( !dupe ) {
 				uint id = purchases++;
-				itemsSold += _itemCount;
-				purchaseId[id] = PurchaseLedger(_timeStamp, _orderNumber, _shopOrderId, _itemCount, _pricePaid);
 				orderNumber[_orderNumber] = id;
-			} 
-		} 
-		*/
-		uint id = purchases++;
-		itemsSold += _itemCount;
-		purchaseId[id] = PurchaseLedger(_timeStamp, _orderNumber, _shopOrderId, _itemCount, _pricePaid);
-		orderNumber[_orderNumber] = id;
+				// check for max 
+				if ( itemsSold + _itemCount > maxItems) {				
+					live = false;
+					stopGameReason = 'MAXED';
+					itemsSold = maxItems;
+					uint itemCountAdjusted = maxItems - itemsSold;
+					uint pricePaidAdjusted = itemCountAdjusted * itemPrice;
+					purchaseId[id] = PurchaseLedger(_timeStamp, _orderNumber, _shopOrderId, itemCountAdjusted, pricePaidAdjusted);
+				} else {
+					itemsSold += _itemCount;
+					purchaseId[id] = PurchaseLedger(_timeStamp, _orderNumber, _shopOrderId, _itemCount, _pricePaid);
+				}
+			}
+		}
 	}
 	// get full purchase ledger 
 	function purchaseLedger() public view returns (uint[] memory, uint[] memory, uint[] memory, uint[] memory, uint[] memory) {
@@ -127,16 +137,16 @@ contract SingleItemSolyd {
 	// ➡️ Player Ledger Functions ➡️
 	//link wallet to player ledger return player id, new player for every new nft minted
 	function newPlayer(uint _orderNumber, address _wallet0x, uint _nft) public {
-
 		uint purchaseLedgerId_ = orderNumber[_orderNumber];
-
-		// 🚧 Check for dupes
-		
-		if ( purchaseId[purchaseLedgerId_].orderNumber != 0) {
+		bool dupe = false;
+		// Test checking for dupes
+		for (uint i = 0; i < players; i++) {
+			if ( playerId[i].purchaseLedgerId == purchaseLedgerId_ ) { dupe = true; }
+		}
+		if ( purchaseId[purchaseLedgerId_].orderNumber != 0 && !dupe ) {
 			uint id = players++;
 			playerId[id] = PlayerLedger(purchaseLedgerId_, _wallet0x, _nft);
 		} 
-
 	}
 	// get full player ledger
 	function playerLedger() public view returns (uint[] memory, address[] memory, uint[] memory) {
@@ -156,17 +166,17 @@ contract SingleItemSolyd {
 	}
 	// ➡️ Payout Functions ➡️
 	//write payout return payout id
-	function writePayout(uint _timeStamp, uint _orderNumber, string memory _payoutMethod, string memory _payoutConfirmation, uint _payoutAmount ) public returns (uint) {
-
-		uint purchaseId_ = orderNumber[_orderNumber];
-		if ( purchaseId_ != 0) {
-			uint id = payments++;
-			payoutId[id] = PayoutLedger(_timeStamp, purchaseId_, _payoutMethod, _payoutConfirmation, _payoutAmount);
-			return id; 
-		} else {
-			return 0;
+	function writePayout(uint _timeStamp, uint _orderNumber, string memory _payoutMethod, string memory _payoutConfirmation, uint _payoutAmount) public {
+		uint purchaseLedgerId_ = orderNumber[_orderNumber];
+		bool dupe = false;
+		for (uint i = 0; i < payments; i++) {
+			if ( payoutId[i].purchaseLedgerId == purchaseLedgerId_ ) { dupe = true; }
 		}
-	} 
+		if ( purchaseId[purchaseLedgerId_].orderNumber != 0 && !dupe) {
+			uint id = payments++;
+			payoutId[id] = PayoutLedger(_timeStamp, purchaseLedgerId_, _payoutMethod, _payoutConfirmation, _payoutAmount);
+		}
+	} //
 	// get unpaid purchase ids 
 	function notPaidoutPurchases() private view returns (uint[] memory) {
 
@@ -182,26 +192,6 @@ contract SingleItemSolyd {
 
 		return (notPaidoutPurchaseIds);
 	}
-	// get unpaid player ids - use this to verify payouts
-	function notPaidoutPlayers() public view returns (address[] memory, uint[] memory) {
-
-		address[] memory wallets_ = new address[](players);
-		uint[] memory pricePaid_ = new uint[](players);
-		uint[] memory notPaidoutPurchaseIds_ = notPaidoutPurchases();
-
-		for (uint i = 1; i <= notPaidoutPurchaseIds_.length; i++) {
-			if (notPaidoutPurchaseIds_[i] != 0){
-				for (uint j = 1; j <= players; j++) {
-					if (playerId[j].purchaseLedgerId == i) {
-						wallets_[j] = playerId[j].walletOx;
-						pricePaid_[j] = (purchaseId[i].pricePaid);
-					}
-				}
-			}
-		}
-
-		return (wallets_, pricePaid_);
-	}
 	// get full payout ledger
 	function payoutLedger() public view returns (uint[] memory, uint[] memory, string[] memory, string[] memory, uint[] memory) {
 
@@ -211,7 +201,7 @@ contract SingleItemSolyd {
 		string[] memory payoutConfirmation_ = new string[](payments);
 		uint[] memory payoutAmount_ = new uint[](payments);
 
-		for (uint i = 1; i <= payments; i++) {
+		for (uint i = 0; i < payments; i++) {
 			timeStamp_[i]=payoutId[i].timeStamp;
 			orderNumber_[i] = purchaseId[payoutId[i].purchaseLedgerId].orderNumber;
 			payoutMedthod_[i] = payoutId[i].payoutMethod;
--- test/solyd-test.js
+++ test/solyd-test.js
@@ -13,39 +13,37 @@ describe("SingleItemSolyd", function() {
 
 		Solyd = await hre.ethers.getContractFactory("SingleItemSolyd");
 		hardhatSolyd = await Solyd.deploy();
+		await hardhatSolyd.writeContractTerms(1648086691106, 'https://', 1000, 10, 20, 5, 5, 1658086691106, 'PAYPALORDERID')
 
 	});	
 
 	describe("Purchase Ledger", function () {
 
 		it("Should return 2 orders in purchase ledger", async function () {
-			
 			await hardhatSolyd.writePurchase(1648086691106, 1001, 231244, 1, 1500);
 			await hardhatSolyd.writePurchase(1648086691106, 1002, 231244, 1, 1500);
-	    	
+			await hardhatSolyd.writePurchase(1648086691106, 1002, 231244, 1, 1500);
 	    	const purchaseLedger = await hardhatSolyd.purchaseLedger();
 	    	expect(purchaseLedger[1].length).to.equal(2);
 			
 		});
 
 		it("Should return 2nd order from purchase ledger", async function () {
-			
 			await hardhatSolyd.writePurchase(1648086691106, 1001, 231244, 1, 1500);
 			await hardhatSolyd.writePurchase(1648086691106, 1002, 231244, 1, 1500);
-	    	
 	    	const purchaseLedger = await hardhatSolyd.purchaseLedger();
 	    	expect(purchaseLedger[1][1]*1).to.equal(1002);
 			
 		});
 
-		it("Should return 5 units from purchase ledger", async function () {
-			
+		it("Should return 10 units from purchase ledger and live status as false and stop reason as MAXED", async function () {
 			await hardhatSolyd.writePurchase(1648086691106, 1001, 231244, 2, 1500);
 			await hardhatSolyd.writePurchase(1648086691107, 1002, 231245, 1, 1500);
-			await hardhatSolyd.writePurchase(1648086691110, 1003, 231246, 2, 1500);
-	    	
+			await hardhatSolyd.writePurchase(1648086691110, 1003, 231246, 20, 1500);
 	    	const status = await hardhatSolyd.gameStatus();
-	    	expect(status[2] * 1).to.equal(5);
+	    	expect(status[2] * 1).to.equal(10);
+	    	expect(status[1]).to.equal(false);
+	    	expect(status[11]).to.equal('MAXED');
 			
 		});
 	});
@@ -56,19 +54,38 @@ describe("SingleItemSolyd", function() {
 			
 			const writePurchaseResult1 = await hardhatSolyd.writePurchase(1648086691106, 1001, 231244, 1, 1500);
 	    	if (writePurchaseResult1.confirmations = 1) {
-	    		const newPlayerResult = await hardhatSolyd.newPlayer(1001, '0x70997970c51812dc3a010c7d01b50e0d17dc79c8', 0);
+	    		await hardhatSolyd.newPlayer(1001, '0x70997970c51812dc3a010c7d01b50e0d17dc79c8', 0);
 	    	}
 	    	const writePurchaseResult2 = await hardhatSolyd.writePurchase(1648086691106, 1002, 231245, 1, 1500);
 	    	if (writePurchaseResult2.confirmations = 1) {
-	    		const newPlayerResult = await hardhatSolyd.newPlayer(1002, '0x3c44cdddb6a900fa2b585dd299e03d12fa4293bc', 0);
+	    		await hardhatSolyd.newPlayer(1002, '0x3c44cdddb6a900fa2b585dd299e03d12fa4293bc', 0);
+	    		await hardhatSolyd.newPlayer(1002, '0x3c44cdddb6a900fa2b585dd299e03d12fa4293bc', 0);
+	    		await hardhatSolyd.newPlayer(1003, '0x3c44cdddb6a900fa2b585dd299e03d12fa4293bc', 0);
+
 	    	}
 	    	const purchaseLedger = await hardhatSolyd.purchaseLedger();
-	    	console.log(purchaseLedger)
 	    	const playerLedger = await hardhatSolyd.playerLedger();
-	    	console.log(playerLedger)
 	    	expect(playerLedger[1].length).to.equal(2);
 			
 		});
 
 	});
+
+	describe("Payout Ledger", function () {
+
+		it("Should return 2 payments in payout ledger", async function () {
+			
+			await hardhatSolyd.writePurchase(1648086691106, 1001, 231244, 1, 1500);
+			await hardhatSolyd.writePurchase(1648086691106, 1002, 231244, 1, 1500);
+			await hardhatSolyd.writePayout(1648086691106, 1001, 'VENMO', 'paypalbatchidconf001', 500)
+			await hardhatSolyd.writePayout(1648086691106, 1002, 'VENMO', 'paypalbatchidconf002', 500)
+			await hardhatSolyd.writePayout(1648086691106, 1002, 'VENMO', 'paypalbatchidconf002', 500)
+			const payoutLedger = await hardhatSolyd.payoutLedger();
+			expect(payoutLedger[1].length).to.equal(2);
+			
+		});
+
+	});
+
+
 });
