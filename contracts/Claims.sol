38e257b91e3ef1518c93ad15d15abd81628e048b
ShadowPuppet66 <andrew@solydaria.co>
2022-06-09 11:08:44 -0400

    claims contract /w shortcut

--- contracts/Claims.sol
+++ contracts/Claims.sol
@@ -10,6 +10,7 @@ contract Claims {
 		address gameContractAddress;
 		uint orderNumber;
 		bool claimed;
+		string shortcut;
 	}
 	mapping (uint => Claim) claimCode;
 	struct ClaimCode {
@@ -18,10 +19,11 @@ contract Claims {
 	mapping (string => ClaimCode) orderStatusUrl;
 	// ➡️ Contract Functions ➡️
 	// write claim
-	function writeClaim(uint _claimCode, address _gameContractAddress, uint _orderNumber, string memory _orderStatusUrl) public {
+	function writeClaim(uint _claimCode, address _gameContractAddress, uint _orderNumber, string memory _orderStatusUrl, string memory _shortcut) public {
 		if (claimCode[_claimCode].orderNumber == 0) {
 			claimCode[_claimCode].gameContractAddress = _gameContractAddress;
 			claimCode[_claimCode].orderNumber = _orderNumber;
+			claimCode[_claimCode].shortcut = _shortcut;
 			orderStatusUrl[_orderStatusUrl].claimCode = _claimCode;
 		}
 	}	
@@ -30,9 +32,9 @@ contract Claims {
 		return(claimCode[_claimCode].gameContractAddress, claimCode[_claimCode].orderNumber, claimCode[_claimCode].claimed);
 	}
 	// get claim from orderStatusUrl
-	function getClaim(string memory _orderStatusUrl) public view returns (address, uint) {
+	function getClaim(string memory _orderStatusUrl) public view returns (address, uint, string memory) {
 		uint claimCode_ = orderStatusUrl[_orderStatusUrl].claimCode;
-		return(claimCode[claimCode_].gameContractAddress, claimCode_);
+		return(claimCode[claimCode_].gameContractAddress, claimCode_, claimCode[claimCode_].shortcut);
 	}
 
 	// claim claim
